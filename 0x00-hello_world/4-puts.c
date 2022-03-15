164e08b2df3ebbf60ac55eae9938452a6f75b9f6
yonathanze <yonathanzemenu@gmail.com>
2022-03-15 10:33:12 -0700

    4th commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <studio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+* **/
+int main(void)
+{
+	puts("\"programming is like building a new multilingual puzzle");
+
+	return (0);
+}
